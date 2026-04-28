8a7501905b3f9d5cb1e584ea1399419d2d08a329
abdulkadirozyurt <92672409+abdulkadirozyurt@users.noreply.github.com>
2026-04-29 00:48:27 +0300

    feat(dal): add database migrations for stocks schema

---
+++ server/FinancialDataTracker/src/FinancialDataTracker.DataAccess/Migrations/20260428120614_mig02_IndexAddedToStockEntity.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialDataTracker.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig02_IndexAddedToStockEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "Stocks",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_Symbol",
+                table: "Stocks",
+                column: "Symbol",
+                unique: true,
+                filter: "[Symbol] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stocks_Symbol",
+                table: "Stocks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "Stocks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
